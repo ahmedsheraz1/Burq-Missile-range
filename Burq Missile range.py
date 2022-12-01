fdf5a5ff7968dc5b32ff10162622f2b70f4b2911
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-01 11:58:28 +0500

    Create Burq Missile range.py

---
+++ Burq Missile range.py
@@ -0,0 +1,11 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+# Measure some strings:
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
+
+    
+cat 3
+window 6
+defenestrate 12
